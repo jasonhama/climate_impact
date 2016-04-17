07a7b5732cac26cbcdb1b7808958a01e4700910e
jasonhama <jjhama@uw.edu>
2016-04-16 17:46:34 -0700

    buttons linked on sign up and sign in

--- climateimpact/app/src/main/java/edu/uw/jjhama/climateimpact/MainActivity.java
+++ climateimpact/app/src/main/java/edu/uw/jjhama/climateimpact/MainActivity.java
@@ -1,13 +1,42 @@
 package edu.uw.jjhama.climateimpact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+//this class handles signing in
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Log.v(TAG, "Hello! ==========================================================");
+            }
+        });
+
+        Button signin = (Button) findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Log.v(TAG, "Helasdfasdfasdfasdflo! ==========================================================");
+
+                //send the user to the signin page
+                Intent intent = new Intent(MainActivity.this, edu.uw.jjhama.climateimpact.Signin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- climateimpact/app/src/main/java/edu/uw/jjhama/climateimpact/Signin.java
+++ climateimpact/app/src/main/java/edu/uw/jjhama/climateimpact/Signin.java
@@ -1,7 +1,43 @@
 package edu.uw.jjhama.climateimpact;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by iguest on 4/15/16.
  */
-public class Signin {
+public class Signin extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Log.v(TAG, "Hello! ==========================================================");
+            }
+        });
+
+        Button signin = (Button) findViewById(R.id.signup);
+        signin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Log.v(TAG, "Helasdfasdfasdfasdflo! ==========================================================");
+
+                //send the user to the signin page
+                Intent intent = new Intent(Signin.this, edu.uw.jjhama.climateimpact.MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
