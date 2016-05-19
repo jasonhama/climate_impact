954c75706a29743efdc50238fac3f6cd5fc6f9d5
Emnet Gossaye <emnetg@uw.edu>
2016-05-18 22:50:59 -0700

    Initial parser

--- web/monday.js
+++ web/monday.js
@@ -3,11 +3,47 @@ var request = require('request');           //request URLs
 var htmlparser = require('htmlparser2');    //parse html
 var cheerio = require('cheerio');
 
-var $ = cheerio.load('<h2 class="title">Hello</h2>');
 
-$('h2.title').text('Hello, World!');
-$('h2').addClass('welcome');
+// var incarbonFP = false;
 
-var s = $.html();
-console.log(s);
+var baseURL = 'http://impact.brighterplanet.com/models';
+var inModelTag = false;
+var footprintVal;
 
+console.log('Scraping URL: ' + baseURL);
+console.log('Initializing parser...');
+var parser = new htmlparser.WritableStream({
+    //called whenever the parser encounters an open tag
+    onopentag: function (name, attrs) {
+        //detect shit about the tag
+        if (name === 'a') {
+            console.log("Found a link tag");
+            console.dir(attrs);
+        }
+    },
+    ontext: function(text) {
+        //if we are in the carbon footprint elem
+        if (inModelTag) {
+            //ontext may be called several times, so append
+            //the text value to req.body.title, or set
+            //req.body.title if it doesn't exist yet
+            console.log(text);
+        }
+    },
+    onclosetag: function() {
+        //once the tag closes we are no longer
+        //in the title element by definition
+        // incarbonFP = false;
+        inModelTag = false;
+    }
+}, {decodeEntities: true});
+
+request.get(targetURL, {followRedirect: false})
+    .on('error', function() {
+        console.error('error requesting page');
+        exit(1);
+    })
+    .on('end', function() {
+        console.log('passing on to parser...');
+    })
+    .pipe(parser);
